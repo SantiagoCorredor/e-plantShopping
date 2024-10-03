dc645c02dc0505a0dc732e7fbe037b211fb99c0d
SantiagoCorredor <santiagocorredor9@gmail.com>
2024-10-03 14:34:33 -0500

    Update

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,6 @@
 import React, { useState,useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { addItem, removeItem } from './cartSlice';
+import { addItem } from './cartSlice';
 import './ProductList.css'
 import CartItem from './CartItem';
 
